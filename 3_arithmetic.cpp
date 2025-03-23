729c4495b1a6095b1c7e54fa5049a2229b9fa1f7
mac <mac@macs-MacBook-Pro.local>
2025-03-23 09:25:06 +0500

    Precedence

--- 3_arithmetic.cpp
+++ 3_arithmetic.cpp
@@ -4,14 +4,32 @@ using namespace std;
 int main() {
     // Arithmetic Operators
     // +, -, *, /, %
+
     // Precedence
     // 1. ()
     // 2. *, /, %
     // 3. +, -
     // =
     // Task; Calculate Currency Notes using remainder function
+    // y = mx+c
+    // y = m*x + c - (a+b) ;
 
-    cout<<"Hello World:"<<endl;
-    cout<<"************";
+    int num1 = 10;
+    int num2 = 22;
+    int y=1, m=9,x=6,c=3,a=4,b=2;
+    y = m*x + c - (a+b); 
+    cout<<"Value of y == "<<y<<endl;
+
+    // 54880; using Modulus
+
+    // 5000 Notes 
+    // 1000 Notes
+    // 500 Note 
+    // 100 Note
+    // 50 Note
+    // 20 
+    // 10
+
+
     return 0;
 }
