076bb1644ff76cdcec17caccf195b97b05e33140
mac <mac@macs-MacBook-Pro.local>
2025-03-23 07:35:41 +0500

    Adding 2 Numbers

--- firstcode.cpp
+++ firstcode.cpp
@@ -3,9 +3,16 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello, World!" << endl;
-    cout << "************" << endl;
-
+    int number1, number2, sum;
+    cout<<"Enter firt Number"<<endl;
+    cin>>number1;
+    cout<<"Enter 2nd Number"<<endl;
+    cin>>number2;
+    sum=number1+number2;
+    cout<<"Sum of 2 Number is = ";
+    cout<<sum<<endl;
     return 0;
+    // +, -, *,/
+    // 3 numbers ko Mul, Div, Add, Sub
 
 }
